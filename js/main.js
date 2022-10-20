0b2cbe57650e66ddf3f6a654402489a973a41ca8
Timofey Mahnev <mahnevtim@sbsit.com>
2022-10-20 17:47:58 +0300

    feedback modal update

--- js/main.js
+++ js/main.js
@@ -114,3 +114,12 @@ document.addEventListener('DOMContentLoaded', function () {
   prepareLightBoxes();
 
 }, false);
+
+document.addEventListener('keyup', function(e) {
+  if(e.key === "Escape") {
+      const modals = document.querySelectorAll('.modal-overlay');
+      for (const modal of modals) {
+          modal.click();
+      }
+  }
+});
